0a421bd0409bb00430a3b5424dc1440248e2d70e
Hamza-MAhmed <hamaahmedmemon01@gmail.com>
2024-01-21 05:22:14 +0500

    class project

---
+++ contact_management_app/server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+require('dotenv').config();
+
+const connectDB = require('./DB');
+
+const app = express()
+
+app.use(express.json({ extended: false }));
+
+// connect db
+connectDB();
+
+// routes
+app.use('/api/v1/users/', require('./routes/users'));
+app.use('/api/v1/auth/', require('./routes/auth'));
+app.use('/api/v1/contacts/', require('./routes/contacts'));
+
+// port
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Server is runing ${PORT}`));
